8946760da8f707a5bda1bceafd34ef0ebf54fba8
Hirendra77 <119104906+Hirendra77@users.noreply.github.com>
2023-01-13 21:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let count;
+document.getElementById("evaluatedText").value="";
+
+document.addEventListener("keyup", function(){
+	count=document.getElementbyId("evaluatedText").value;
+
+	document.querySelector("#letterCount").textContent=count.length;
+});
